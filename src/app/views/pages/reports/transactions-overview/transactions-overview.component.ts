8555c5d2f2e01c869a43e5c2a69897e27ff65b24
Yannis Stebler <yannis.stebler@bbzbl-it.ch>
2025-05-15 14:34:43 +0200

    implemented Overview for Transactions without functions

--- src/app/views/pages/reports/transactions-overview/transactions-overview.component.ts
+++ src/app/views/pages/reports/transactions-overview/transactions-overview.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { Transaction } from '../../../../models/transaction.model';
+import { Category } from '../../../../models/category.model';
+import { CategoryService } from '../../../../services/category.service';
+import { TransactionService } from '../../../../services/transaction.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transactions-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './transactions-overview.component.html',
-  styleUrl: './transactions-overview.component.scss'
+  styleUrl: './transactions-overview.component.scss',
 })
 export class TransactionsOverviewComponent {
+  transactions: Transaction[] = [];
+  categories: Category[] = [];
 
+  constructor(
+    private transactionService: TransactionService,
+    private categoryService: CategoryService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadTransactions();
+    this.loadCategories();
+  }
+
+  loadTransactions(): void {
+    this.transactionService.getAllTransactions().subscribe((data) => {
+      this.transactions = data;
+    });
+  }
+
+  loadCategories(): void {
+    this.categoryService.getAllCategories().subscribe((data) => {
+      this.categories = data;
+    });
+  }
+
+  getCategoryName(categoryId: number): string {
+    const category = this.categories.find((cat) => cat.id === categoryId);
+    return category ? category.name : 'Unknown';
+  }
 }
